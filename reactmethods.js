6a02eb3781ad1e5217a86d925392f0f5ce8fb91b
Paramjeet <paramjeetnpradhan@gmail.com>
2023-12-12 20:09:53 +0530

    filter,reduce

--- reactmethods.js
+++ reactmethods.js
@@ -1,8 +1,29 @@
 
-let numbers=[2,3,4,1,24,1344];
+const numbers=[2,3,4,1,24,1344];
+
+
 //map
-let newnumber=[];
-numbers.map((num)=>{
-newnumber.push(num*2);
+// let newnumber=[];
+// numbers.map((num)=>{
+// newnumber.push(num*2);
+// })
+// console.log(newnumber);
+
+//filter
+// let filternumber=[]
+// numbers.filter((num)=>{
+//     if(num>3){
+//         filternumber.push(num);
+//     }
+
+// })
+// console.log(filternumber);
+
+
+
+//reduce: acumulate a value by doing something to each item in a array
+
+var newreduce=numbers.reduce((accumulator,currentNumber)=>{
+return accumulator+currentNumber;
 })
-console.log(newnumber);
+console.log(newreduce);
